0ee1160d2117e50cdfb7c97c0ee0a9218a3c9e07
jeremyben-meir <jsb459@cornell.edu>
2022-07-05 01:00:08 -0400

    website commit dump

---
+++ src/styles/HomeStyles.js
@@ -0,0 +1,133 @@
+import background from "./../../src/assets/background.jpg";
+const navHeight = "60px";
+
+export const MainDiv = {
+    display:"flex",
+    width:'100%',
+    height:'100%',
+    position: "absolute",
+    flexDirection:"column",
+
+    fontFamily: "MyFont",
+}
+export const ScrollDiv = {
+    display:"flex",
+    width:'100%',
+    height:'100%',
+    flexDirection:"column",
+    overflow:"scroll",
+    marginBottom:navHeight,
+    // border:"solid red 10px"
+}
+
+export const NavDiv = {
+    display:"flex",
+    width:"100%",
+    height:"25%",
+    minHeight:"25%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    textAlign:"center",
+}
+
+export const SubNavDiv = {
+    display:"flex",
+    flex:"1",
+    height:"100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    boxSizing:"border-box",
+    padding:"0px 20px 0px 20px",
+    // border:"solid purple"
+}
+
+export const TextDiv = {
+    display:"flex",
+    flex:"9",
+    height:"100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+}
+
+export const ImgDiv = (wide) => ({
+    display:"flex",
+    width:"100%",
+    height:"75%",
+    minHeight:"75%",
+    flexDirection:wide ? "row" : "column",
+    // border:"solid green 1px"
+})
+
+export const SubImgDiv = {
+    display:"flex",
+    flex:"2",
+    alignItems:"center",
+    overflow:"hidden",
+    minWidth:"50%",
+    // border:"solid red"
+}
+
+export const SubImgDiv2 = {
+    display:"flex",
+    flex:"1",
+    flexDirection:"row",
+    fontSize: "13px",
+    justifyContent:"center",
+    alignItems:"center",
+    padding:"30px",
+    boxSizing:"border-box",
+    minWidth:"50%",
+    // border:"solid red"
+}
+
+export const ImgStyle = {
+    width:"100%",
+    height:"100%",
+    objectFit: "cover",
+}
+
+export const HeaderDiv = {
+    fontSize: "50px",
+    fontWeight: "bold",
+    // border:"solid green"
+}
+
+export const SubHeaderDiv = {
+    fontSize: "20px",
+    fontWeight: "normal",
+    // border:"solid red"
+}
+
+export const PopupStyle = {
+    display:"flex",
+    flexDirection:"column",
+    backgroundColor:"white",
+    borderRadius:"10px",
+    boxSizing:"border-box",
+    padding:"30px",
+    alignItems:"flex-start",
+    justifyContent:"center",
+    gap:"10px",
+
+}
+
+export const BottomNav = {
+    display:"flex",
+    flexDirection:"row",
+    position:"fixed",
+    backgroundColor:"white",
+    width: "100%",
+    height:navHeight,
+    bottom:"0",
+    boxShadow:"0px 0px 6px 2px rgba(0, 0, 0, .2)",
+    justifyContent:"center",
+    alignItems:"center",
+    gap:"20px"
+}
+
+export const SocialImg = {
+    height:"20px",
+    width:"20px",
+}
---
+++ src/views/Home.js
@@ -0,0 +1,94 @@
+import React, {useEffect, useState} from "react";
+import * as HomeStyles from "../styles/HomeStyles";
+import Popup from 'reactjs-popup';
+import background from "../assets/background.jpg";
+import twitter from "../assets/social/twitter.png";
+import soundcloud from "../assets/social/soundcloud.png";
+import instagram from "../assets/social/instagram.png";
+import spotify from "../assets/social/spotify.png";
+
+const Home = () => {
+
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    useEffect(() => {
+        function handleWindowResize() {
+          setWindowSize(getWindowSize());
+        }
+    
+        window.addEventListener('resize', handleWindowResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    function getWindowSize() {
+        const {innerWidth, innerHeight} = window;
+        return {innerWidth, innerHeight};
+    }
+
+    return(
+        <div style={HomeStyles.MainDiv}>
+            <div style={HomeStyles.ScrollDiv}>
+                <div style={HomeStyles.NavDiv}>
+                    <Popup modal nested
+                        trigger={ <div style={HomeStyles.SubNavDiv}> Contact </div>} 
+                        >
+                        <div style={HomeStyles.PopupStyle}>
+                            <div>
+                                <b>Phone</b> (917) 238-6997 <br/>
+                            </div>
+                            <div>
+                                <b>Email</b> samdbenmeir@gmail.com
+                            </div>
+                        </div>
+                    </Popup>
+                    <div style={HomeStyles.TextDiv}>
+                        <div style={HomeStyles.HeaderDiv}>
+                            Sam Ben-Meir
+                        </div>
+                        <div style={HomeStyles.SubHeaderDiv}>
+                            Singer, Songwriter, Instrumentalist
+                        </div>
+                    </div>
+                    <div style={HomeStyles.SubNavDiv} onClick={()=>document.getElementById('scrollable').scrollIntoView({ behavior: 'smooth' })}>
+                        Music
+                    </div>
+                </div>
+                <div style={HomeStyles.ImgDiv(windowSize.innerWidth>800)}>
+                    <div style={HomeStyles.SubImgDiv}>
+                        <img src={background} style={HomeStyles.ImgStyle}/>
+                    </div>
+                    <div style={HomeStyles.SubImgDiv2}>
+                        <div>
+                            Sam Ben-Meir is a multi-faceted instrumentalist and composer. Originally from St. Louis, Missouri, Sam started playing the violin at 5 years of age. At 7 years, Sam and his family moved to New York where he continued to immerse himself in music, including learning to play the piano, double-bass and guitar. Sam later studied jazz at the University of Miami, and performed regularly at local venues.
+                            <br/><br/>
+                            In 2006, Sam returned to New York City to play restaurants and clubs while earning a PhD degree in Philosophy at the New School for Social Research. Throughout his career, Sam has worked with numerous noted and legendary musical talents. Sam currently lives on Manhattan’s Upper West Side where he passionately pursues his love of music as a singer, songwriter and composer.
+                        </div>
+                    </div>
+                </div>
+                <div id="scrollable" style={HomeStyles.ImgDiv(windowSize.innerWidth>800)}>
+                    <div style={HomeStyles.SubImgDiv}>
+                        <img src={background} style={HomeStyles.ImgStyle}/>
+                    </div>
+                    <div style={HomeStyles.SubImgDiv2}>
+                        <div>
+                            Sam Ben-Meir is a multi-faceted instrumentalist and composer. Originally from St. Louis, Missouri, Sam started playing the violin at 5 years of age. At 7 years, Sam and his family moved to New York where he continued to immerse himself in music, including learning to play the piano, double-bass and guitar. Sam later studied jazz at the University of Miami, and performed regularly at local venues.
+                            <br/><br/>
+                            In 2006, Sam returned to New York City to play restaurants and clubs while earning a PhD degree in Philosophy at the New School for Social Research. Throughout his career, Sam has worked with numerous noted and legendary musical talents. Sam currently lives on Manhattan’s Upper West Side where he passionately pursues his love of music as a singer, songwriter and composer.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={HomeStyles.BottomNav}>
+                <img src={soundcloud} style={HomeStyles.SocialImg} onClick={() => window.open("https://soundcloud.com/samben-meir?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing")}/>
+                <img src={spotify} style={HomeStyles.SocialImg} onClick={() => window.open("https://open.spotify.com/artist/38RZtz6gGzK5zni0BW60yN?si=HvZ_ua7uQfqnW9Jm-mXNsQ")}/>
+                <img src={twitter} style={HomeStyles.SocialImg} onClick={() => window.open("http://twitter.com/sambenmeir")}/>
+                <img src={instagram} style={HomeStyles.SocialImg} onClick={() => window.open("https://www.instagram.com/sam.benmeir/")}/>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
